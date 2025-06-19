af0f9287d0248306b260eeb5c4cdcdcd5fa621da
Tim Prince <tim.prince@gmail.com>
2025-06-19 12:19:36 -0700

    Tweak types and fix some pylint.

--- src/rss_slicer/__init__.py
+++ src/rss_slicer/__init__.py
@@ -1,25 +1,60 @@
+"""
+RSS slicer library
+~~~~~~~~~~~~~~~~~~
+
+`rss-slicer` is an RSS feed slicer-dicer for combining an arbitrary
+set of input RSS feeds and transforming them into a new set of output
+streams.
+
+:copyright: (c) 2025 by Tim Prince
+:license: GPL v3, see COPYING for details.
+"""
+from typing import Annotated, Callable
 from lxml.etree import ElementTree as Element
-from dataclasses import dataclass
-from typing import Callable
 
 
-@dataclass
-class Mutation:
-    query: str
-    mutator: Callable[[Element], bool]
+Callback = Annotated[
+    Callable[[Element], bool],
+    """\
+Bundles together an XPath query and a function
+that modifies an XML element. The function should
+modify the passed-in Element in-place and return
+`True` if the element (and its children) should be
+deleted from the tree and `False` if it should be
+preserved.
+"""
+]
 
 
-def apply_one_mutation(tree: Element, mutation: Mutation):
-    nodes = tree.findall(mutation.query)
+def apply_one_mutation(tree: Element, query: str, mutator: Callback):
+    """Given a mutation, apply it to the children of `tree` that
+    satisfy the associated XPath query. The tree is modified in-place.
+
+    :param tree: the root XML node to which tthe transformation should be
+        applied.
+    :param query: the XPath query to select elements which should be
+        mutated.
+    :param mutator: the mutation function to apply to the tree.
+    :rtype: None
+    """
+    nodes = tree.findall(query)
     to_remove = []
     for node in nodes:
-        if mutation.mutator(node):
+        if mutator(node):
             to_remove.append(node)
 
     for node in to_remove:
         node.find('..').remove(node)
 
 
-def apply_mutations(tree: Element, mutations: list[Mutation]):
+def apply_mutations(tree: Element, mutations: list[tuple[str, Callback]]):
+    """Given a list of mutations, apply them in sequence to
+    the given tree.
+
+    :param tree: the root XML node to which the transformations should be
+        applied.
+    :param mutations: the mutations to apply to the tree.
+    :rtype: None
+    """
     for mutation in mutations:
-        apply_one_mutation(tree, mutation)
+        apply_one_mutation(tree, *mutation)
--- tests/test_mutate.py
+++ tests/test_mutate.py
@@ -1,21 +1,69 @@
 """Tests for RSS slicing functionality."""
 from lxml import etree
 from lxml.etree import Element
-from rss_slicer import Mutation, apply_one_mutation
+from rss_slicer import apply_one_mutation, apply_mutations, Callback
+
+
+class TestMutators:
+    """Some basic mutators for use in unit tests."""
+
+    @staticmethod
+    def retag(new_tag: str) -> Callback:
+        """Make a mutator that replaces all tags with `new_tag`."""
+        def cb(e: Element) -> bool:
+            e.tag = new_tag
+            return False
+
+        return cb
+
+    @staticmethod
+    def oneshot(mutator: Callback) -> Callback:
+        """Make a mutator that runs only once."""
+        flag = False
+
+        def cb(e: Element) -> bool:
+            nonlocal flag
+            if not flag:
+                mutator(e)
+                flag = True
+
+            return False
+
+        return cb
+
+    @staticmethod
+    def delete() -> Callback:
+        """Create a mutator that unconditionally deletes the element."""
+        def cb(_: Element) -> bool:
+            return True
+
+        return cb
+
+    @staticmethod
+    def delete_matching(tag: str) -> Callback:
+        """Create a mutator that deletes any element with a matching tag."""
+        def cb(e: Element) -> bool:
+            return e.tag == tag
+
+        return cb
 
 
 def test_trivial():
     """Trivial smoke test for basic rss_slicer functionality."""
     root = etree.fromstring(b'<data><a/><b/><c/></data>')
 
-    def fn(e: Element) -> bool:
-        if e.tag == 'c':
-            return True
+    apply_one_mutation(
+        root, './',
+        TestMutators.oneshot(TestMutators.retag('d'))
+    )
+    assert b'<data><d/><b/><c/></data>' == etree.tostring(root)
 
-        e.tag = 'd'
-        return False
-
-    mutator = Mutation('./', fn)
-
-    apply_one_mutation(root, mutator)
-    assert b'<data><d/><d/></data>' == etree.tostring(root)
+    apply_mutations(
+        root,
+        [
+            ('./b', TestMutators.retag('e')),
+            ('./', TestMutators.delete_matching('b')),
+            ('./d', TestMutators.delete())
+        ]
+    )
+    assert b'<data><e/><c/></data>' == etree.tostring(root)
